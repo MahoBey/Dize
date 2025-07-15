a5441420be631567b0ba0b3a0b41dac1bcf78948
MahoBey <mahobey2025@gmail.com>
2025-07-16 01:02:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 from os import getenv
 from dotenv import load_dotenv
 
@@ -10,5 +9,5 @@ API_HASH = getenv("33e43e71a6d85dde11988e70b6980a0e")
 BOT_TOKEN = getenv("7995860392:AAGAdTb83LrJ3Bdj3QOC2F2aBUznrvy3W4A")
 OWNER_ID = int(getenv("8044097780"))
 
-MONGO_DB_URI = getenv("mongodb+srv://veysi7074:UTNs8dpY4Fn28mAY@cluster0.nigbbzs.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("mongodb+srv://Mahoaga:<db_1234554321m>@cluster0.8ni5ega.mongodb.net/")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ultroidofficial_chat")
